6c3f6cb41518e3e31ae780ee335c5f2d27014c56
Purushotham Prajapati(Uttam Purush) <purushothamprajapati7473@gmail.com>
2025-07-28 22:36:09 +0530

    Add files via upload

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: [
-      'e73b-103-248-208-99.ngrok-free.app'
+      'a3a85014cadc.ngrok-free.app'
     ]
   }
 })
